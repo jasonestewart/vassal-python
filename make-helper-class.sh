fd836c561504e8e1b41efc31388c46b9dff49a8f
Jason Stewart <jason.e.stewart@gmail.com>
2022-09-07 22:36:27 +0100

    changes to work with intellij

--- make-helper-class.sh
+++ make-helper-class.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-javac -classpath ../vassal/release-prepare/target/lib/Vengine.jar -d classlib/ inc/VASSAL/tools/python/Helper.java
+javac -classpath ./lib/Vengine.jar -d classlib/ src/Helper.java
--- test.py
+++ test.py
@@ -3,17 +3,76 @@ import jpype.imports
 
 jpype.startJVM(
     classpath=[
-        "/Users/jasons/personal-dev/vassal/release-prepare/target/lib/Vengine.jar",
+        "../vassal/release-prepare/target/lib/Vengine.jar",
         "classlib/"
         ])
 
-from VASSAL.tools.python import Helper
+import VASSAL.tools.python.Helper
+import VASSAL.build.module.PieceWindow
+import VASSAL.build.widget.PieceSlot
+import VASSAL.build.Widget
+import VASSAL.build.widget.TabWidget
+import VASSAL.build.widget.PanelWidget
+import VASSAL.build.widget.ListWidget
+import VASSAL.build.widget.BoxWidget
+from IPython import embed
 
-helper = Helper("0.1")
+def printPieces(pw, indent):
+    space = indent * '    '
+    name = pw.getAttributeValueString('name')
+    if not name:
+        pw.getAttributeValueString('entryName')
+    print(f"{space}{name}")
+
+    buildables = list(pw.getBuildables())
+    for b in buildables:
+        if isinstance(b, VASSAL.build.widget.PieceSlot):
+            print(f"{space}{b.getPiece().getName()}")
+        elif isPieceWidget(b):
+            printPieces(b,indent+1)
+        else:
+            print(f"Error: {b.__class__}")
+
+
+def getPieces(pw):
+    pieces = []
+    if isinstance(pw, VASSAL.build.widget.PieceSlot):
+        print("Found piece slot")
+        pieces.append(pw.getPiece().getName())
+    else:
+        buildables = list(pw.getBuildables())
+        for b in buildables:
+            if isPieceWidget(b):
+                pieces.extend(getPieces(b))
+
+    return pieces
+
+def isPieceWindow(pw):
+    return isinstance(pw, VASSAL.build.module.PieceWindow)
+
+def isPieceWidget(pw):
+    return isinstance(pw, (
+        VASSAL.build.module.PieceWindow,
+        VASSAL.build.widget.TabWidget,
+        VASSAL.build.widget.ListWidget,
+        VASSAL.build.widget.PieceSlot,
+        VASSAL.build.widget.PanelWidget,
+        VASSAL.build.widget.BoxWidget)
+    )
+
+
+helper = VASSAL.tools.python.Helper("0.1")
 print(f'Script version: {helper.getPythonVersion()}')
 print(f'Java version: {helper.getJavaVersion()}')
 print(f'VASSAL version: {helper.getVASSALVersion()}')
 
 print("Opening game module")
 gameModule = helper.initGameModule("./test.vmod")
+
+pws = list(filter(isPieceWindow, gameModule.getBuildables()))
+print(f'{pws}')
+
+for panel in pws:
+    printPieces(panel,0)
+
 print("Success!")
