6e78b60a09f0b32b73b4cc0a5f6469f464a2aa8f
Jason Stewart <jason.e.stewart@gmail.com>
2022-09-25 22:42:11 +0100

    path fixes

--- tools/make-helper-class.sh
+++ tools/make-helper-class.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 VASSAL_PYTHON_HOME="${HOME}/work/vassal-python"
-javac -classpath ${VASSAL_PYTHON_HOME}/lib/java/Vengine.jar \
+VASSAL_HOME="${HOME}/Documents/VASSAL/VASSAL-3.6.4"
+javac -classpath "${VASSAL_HOME}/lib/*" \
     -d ${VASSAL_PYTHON_HOME}/lib/java/ ${VASSAL_PYTHON_HOME}/src/java/Helper.java
